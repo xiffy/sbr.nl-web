06b8d7f6fee297d2fb150aca35ab1e7e04588cd6
xiffy <xiffy@xiffy.nl>
2024-03-11 18:04:18 +0100

    but does the index get rewritten?

--- scripts/html_index.py
+++ scripts/html_index.py
@@ -22,7 +22,7 @@ class HTMLIndex:
             root = root.replace(f"{self.directory}/", "")
             output += template.render({"root": root, "dirs": dirs, "files": files})
         result = index.replace("@@@", output)
-        print(result)
+        print(result)  # just catch disk and write to disk
 
 
 def main():
