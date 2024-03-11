8c57812287eddc2066f12e4b34d437b67457a07f
xiffy <xiffy@xiffy.nl>
2024-03-11 16:05:10 +0100

    publiceer welke zip-packaes we hebben

---
+++ scripts/html-index.py
@@ -0,0 +1,34 @@
+import os
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+env = Environment(
+    loader=FileSystemLoader("../templates"),
+    autoescape=select_autoescape(["html", "xml"]),
+)
+
+
+class HTMLIndex:
+    def __init__(self, directory):
+        self.directory = directory
+
+    def render_html(self):
+        indextemplate = env.get_template("index.html")
+        template = env.get_template("dir.html")
+
+        index = indextemplate.render()
+        output = ""
+        for root, dirs, files in os.walk(self.directory):
+            if root.endswith("/public"):
+                continue
+            root = root.replace("../public/", "")
+            output += template.render({"root": root, "dirs": dirs, "files": files})
+        result = index.replace("@@@", output)
+        print(result)
+
+
+def main():
+    index = HTMLIndex("../public")
+    index.render_html()
+
+
+if __name__ == "__main__":
+    main()
